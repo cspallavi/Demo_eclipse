d859f1e785a8abf0c419920d0ad0835cdb3e538f
cspallavi <pallaviagrawal79@gmail.com>
2018-08-27 00:17:51 +0530

    changed printing argument again

--- src/Abc.java
+++ src/Abc.java
@@ -6,7 +6,7 @@ class Abc {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello People  again:)");
+		System.out.println("Hello People once again :)");
 
 	}
 
